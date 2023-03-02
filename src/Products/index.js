29f5b7c7ea6e9b412a8392650f4b2ab0bbb7ecef
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-03-03 00:15:58 +0500

    new

--- src/Products/index.js
+++ src/Products/index.js
@@ -10,7 +10,7 @@ import { useNavigate } from "react-router-dom";
 
 const Products = () => {
     useEffect(() => {
-        document.title = "Товары";
+        document.title = "Товары 2";
     }, []);
 
     const navigate = useNavigate();
